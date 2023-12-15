c4a413f618006cfb2475d65a4acc9f4bd426ccf2
Hamish Croser <hamish.croser@sydney.edu.au>
2023-12-15 15:14:05 +1100

    Added TSV file type support Closes #6

--- corpusloader/controller/file_loader_strategy/concrete_strategies/CSVLoaderStrategy.py
+++ corpusloader/controller/file_loader_strategy/concrete_strategies/CSVLoaderStrategy.py
@@ -14,7 +14,7 @@ class CSVLoaderStrategy(FileLoaderStrategy):
             trimmed_f.write(f.readline())
         trimmed_f.seek(0)
 
-        df: DataFrame = read_csv(trimmed_f)
+        df: DataFrame = read_csv(trimmed_f, header=0)
         headers: list[CorpusHeader] = []
         for header_name, dtype_obj in df.dtypes.items():
             dtype_str: str = str(dtype_obj).upper()
@@ -28,7 +28,7 @@ class CSVLoaderStrategy(FileLoaderStrategy):
         return headers
 
     def get_dataframe(self, headers: list[CorpusHeader]) -> DataFrame:
-        df: DataFrame = read_csv(self.filepath)
+        df: DataFrame = read_csv(self.filepath, header=0)
         dtypes_applied_df: DataFrame = FileLoaderStrategy._apply_selected_headers(df, headers)
 
         return dtypes_applied_df
--- corpusloader/controller/file_loader_strategy/concrete_strategies/TSVLoaderStrategy.py
+++ corpusloader/controller/file_loader_strategy/concrete_strategies/TSVLoaderStrategy.py
@@ -1,12 +1,34 @@
-from pandas import DataFrame
+from io import StringIO
 
-from corpusloader.controller.data_objects import CorpusHeader
+from pandas import DataFrame, read_csv
+
+from corpusloader.controller.data_objects import CorpusHeader, DataType
 from corpusloader.controller.file_loader_strategy.FileLoaderStrategy import FileLoaderStrategy
 
 
 class TSVLoaderStrategy(FileLoaderStrategy):
     def get_inferred_headers(self) -> list[CorpusHeader]:
-        pass
+        trimmed_f = StringIO()
+        with open(self.filepath) as f:
+            trimmed_f.write(f.readline())
+            trimmed_f.write(f.readline())
+        trimmed_f.seek(0)
 
-    def get_dataframe(self) -> DataFrame:
-        pass
+        df: DataFrame = read_csv(trimmed_f, sep='\t', header=0)
+        headers: list[CorpusHeader] = []
+        for header_name, dtype_obj in df.dtypes.items():
+            dtype_str: str = str(dtype_obj).upper()
+            dtype: DataType
+            try:
+                dtype = DataType[dtype_str]
+            except KeyError:
+                dtype = DataType['STRING']
+            headers.append(CorpusHeader(str(header_name), dtype, True))
+
+        return headers
+
+    def get_dataframe(self, headers: list[CorpusHeader]) -> DataFrame:
+        df: DataFrame = read_csv(self.filepath, sep='\t', header=0)
+        dtypes_applied_df: DataFrame = FileLoaderStrategy._apply_selected_headers(df, headers)
+
+        return dtypes_applied_df
