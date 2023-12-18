1e78014eeec3718426366acd53e2dd2f9331540d
Hamish Croser <hamish.croser@sydney.edu.au>
2023-12-18 11:32:03 +1100

    Added XLSX file type support Closes #7

--- corpusloader/controller/file_loader_strategy/concrete_strategies/XLSXLoaderStrategy.py
+++ corpusloader/controller/file_loader_strategy/concrete_strategies/XLSXLoaderStrategy.py
@@ -1,12 +1,27 @@
-from pandas import DataFrame
+from pandas import DataFrame, read_excel
 
-from corpusloader.controller.data_objects import CorpusHeader
+from corpusloader.controller.data_objects import CorpusHeader, DataType
 from corpusloader.controller.file_loader_strategy.FileLoaderStrategy import FileLoaderStrategy
 
 
 class XLSXLoaderStrategy(FileLoaderStrategy):
     def get_inferred_headers(self) -> list[CorpusHeader]:
-        pass
+        df: DataFrame = read_excel(self.filepath, nrows=2)
+        headers: list[CorpusHeader] = []
+        for header_name, dtype_obj in df.dtypes.items():
+            dtype_str: str = str(dtype_obj).upper()
+            dtype: DataType
+            try:
+                dtype = DataType[dtype_str]
+            except KeyError:
+                dtype = DataType['STRING']
+            headers.append(CorpusHeader(str(header_name), dtype, True))
 
-    def get_dataframe(self) -> DataFrame:
-        pass
+        return headers
+
+    def get_dataframe(self, headers: list[CorpusHeader]) -> DataFrame:
+        included_headers: list[str] = [header.name for header in headers if header.include]
+        df: DataFrame = read_excel(self.filepath, header=0, names=included_headers)
+        dtypes_applied_df: DataFrame = FileLoaderStrategy._apply_selected_dtypes(df, headers)
+
+        return dtypes_applied_df
