306c838ad6fda7819246cb846d3adb3a2738a26c
Hamish Croser <hamish.croser@sydney.edu.au>
2024-01-12 14:39:07 +1100

    Hidden files (files with names beginning with a '.') are omitted by default and a checkbox to show hidden files was added

--- corpusloader/view/gui/FileSelectorWidget.py
+++ corpusloader/view/gui/FileSelectorWidget.py
@@ -2,7 +2,7 @@ from fnmatch import fnmatch
 
 import panel
 from panel import Row, Column
-from panel.widgets import Button, MultiSelect, TextInput, Select
+from panel.widgets import Button, MultiSelect, TextInput, Select, Checkbox
 
 from corpusloader.controller import Controller
 from corpusloader.controller.data_objects import FileReference
@@ -23,6 +23,9 @@ class FileSelectorWidget(AbstractWidget):
                                       sizing_mode='stretch_width')
         self.filter_input.param.watch(self._on_filter_change, ['value_input'])
 
+        self.show_hidden_files_checkbox = Checkbox(name="Show hidden", value=False, align="center")
+        self.show_hidden_files_checkbox.param.watch(self._on_filter_change, ['value'])
+
         self.file_type_filter = Select(width=150)
         self.file_type_filter.options = ['All valid filetypes'] + self.controller.get_valid_filetypes()
         self.file_type_filter.param.watch(self._on_filter_change, ['value'])
@@ -32,6 +35,7 @@ class FileSelectorWidget(AbstractWidget):
         self.panel = Column(
             Row(self.select_all_button),
             Row(self.filter_input,
+                self.show_hidden_files_checkbox,
                 self.file_type_filter),
             Row(self.selector_widget),
             width=700)
@@ -41,23 +45,11 @@ class FileSelectorWidget(AbstractWidget):
         self.select_all()
 
     def update_display(self):
-        all_file_refs: list[FileReference] = self.controller.retrieve_all_files()
         loaded_corpus_files: list[FileReference] = self.controller.get_loaded_corpus_files()
         loaded_meta_files: list[FileReference] = self.controller.get_loaded_meta_files()
 
-        valid_file_types: list[str] = self.controller.get_valid_filetypes()
-        selected_file_types: list[str]
-        if self.file_type_filter.value in valid_file_types:
-            selected_file_types = [self.file_type_filter.value.upper()]
-        else:
-            selected_file_types = [ft.upper() for ft in valid_file_types]
-
-        filter_str = f"*{self.filter_input.value_input}*"
-        filtered_refs: list[FileReference] = []
-        for ref in all_file_refs:
-            extension: str = ref.get_extension().upper()
-            if fnmatch(ref.get_relative_path(), filter_str) and (extension in selected_file_types):
-                filtered_refs.append(ref)
+        all_file_refs: list[FileReference] = self.controller.retrieve_all_files()
+        filtered_refs: list[FileReference] = self._filter_file_refs(all_file_refs)
 
         old_selected_refs: list[FileReference] = self.selector_widget.value.copy()
         filtered_selected_refs: list[FileReference] = [f for f in old_selected_refs if f in filtered_refs]
@@ -74,6 +66,30 @@ class FileSelectorWidget(AbstractWidget):
 
         self.selector_widget.options = filtered_files_dict
         self.selector_widget.value = filtered_selected_refs
+
+    def _filter_file_refs(self, file_refs: list[FileReference]) -> list[FileReference]:
+        filtered_refs: list[FileReference] = []
+
+        valid_file_types: list[str] = self.controller.get_valid_filetypes()
+        selected_file_types: list[str]
+        if self.file_type_filter.value in valid_file_types:
+            selected_file_types = [self.file_type_filter.value.upper()]
+        else:
+            selected_file_types = [ft.upper() for ft in valid_file_types]
+
+        filter_str = f"*{self.filter_input.value_input}*"
+        for ref in file_refs:
+            if not fnmatch(ref.get_relative_path(), filter_str):
+                continue
+            if ref.get_extension().upper() not in selected_file_types:
+                continue
+            is_hidden_file: bool = ref.get_filename().startswith('.')
+            if is_hidden_file and not self.show_hidden_files_checkbox.value:
+                continue
+
+            filtered_refs.append(ref)
+
+        return filtered_refs
 
     def _on_filter_change(self, *_):
         self.update_display()
