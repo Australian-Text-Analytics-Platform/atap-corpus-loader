944b01f521c2d7c77f9338d720b3fbef59fb7387
Hamish Croser <hamish.croser@sydney.edu.au>
2024-02-08 09:14:30 +1100

    Moved corpus metadata info display

--- atap_corpus_loader/view/gui/CorpusInfoWidget.py
+++ atap_corpus_loader/view/gui/CorpusInfoWidget.py
@@ -17,9 +17,14 @@ class CorpusInfoWidget(AbstractWidget):
         super().__init__()
         self.controller: Controller = controller
 
-        self.panel = Accordion(toggle=True, min_width=700)
-        self.panel.header_background = "#FFFFFA"
-        self.panel.active_header_background = "#7A8B99"
+        self.corpus_controls = Accordion(toggle=True, min_width=700)
+        self.corpus_controls.header_background = "#FFFFFA"
+        self.corpus_controls.active_header_background = "#7A8B99"
+        self.corpus_controls.param.watch(self._update_corpus_display, 'active', onlychanged=False)
+
+        self.corpus_display: Markdown = Markdown()
+
+        self.panel = Row(self.corpus_controls, self.corpus_display)
 
     @staticmethod
     def _build_corpus_label(corpus_info: ViewCorpusInfo) -> str:
@@ -52,23 +57,40 @@ class CorpusInfoWidget(AbstractWidget):
         self.controller.delete_corpus(corpus_id)
         self.update_display()
 
-    def update_display(self):
-        corpora_info: list[ViewCorpusInfo] = self.controller.get_corpora_info()
-        self.panel.objects = []
-        if len(corpora_info) == 0:
+    def _update_corpus_display(self, *event):
+        current_active: list[int] = self.corpus_controls.active
+        if len(current_active) == 0:
+            self.corpus_display.object = ' '
+            self.corpus_display.visible = False
             return
 
-        accordion_objs: list[Row] = []
+        corpora_info: list[ViewCorpusInfo] = self.controller.get_corpora_info()
+        active_idx: int = current_active[0]
+        if active_idx >= len(corpora_info):
+            self.corpus_display.object = ' '
+            self.corpus_display.visible = False
+            return
+
+        corpus_info: ViewCorpusInfo = corpora_info[active_idx]
+        header_markdown_table: str = CorpusInfoWidget._build_header_markdown_table(corpus_info)
+        corpus_display_text: str = f"**<div style='text-align: center;'>{corpus_info.name}</div>**\n\n{header_markdown_table}"
+
+        self.corpus_display.object = corpus_display_text
+        self.corpus_display.visible = True
+
+    def update_display(self):
+        corpora_info: list[ViewCorpusInfo] = self.controller.get_corpora_info()
+
+        corpus_controls_objs: list[Row] = []
         for corpus_info in corpora_info:
             label: str = CorpusInfoWidget._build_corpus_label(corpus_info)
-            header_markdown_table: str = CorpusInfoWidget._build_header_markdown_table(corpus_info)
-            info_markdown: Markdown = Markdown(header_markdown_table)
 
             rename_field: TextInput = TextInput(name="Rename corpus", value=corpus_info.name, align='center')
             rename_field.param.watch(lambda event, corpus_id=corpus_info.corpus_id: self.rename_corpus(corpus_id, event.new), ['value'])
             delete_button: Button = Button(name="Delete corpus", button_type="danger", align='center')
             delete_button.on_click(lambda event, corpus_id=corpus_info.corpus_id: self.delete_corpus(corpus_id))
 
-            accordion_objs.append(Row(info_markdown, HSpacer(), rename_field, delete_button, name=label))
+            corpus_controls_objs.append(Row(rename_field, delete_button, name=label))
 
-        self.panel.objects = accordion_objs
+        self.corpus_controls.objects = corpus_controls_objs
+        self._update_corpus_display()
