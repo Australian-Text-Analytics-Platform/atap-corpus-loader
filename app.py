51f6d1ebeb9879ba0c30c21e83dec4580307d512
Hamish Croser <h.croser1@gmail.com>
2024-10-18 16:11:56 +1100

    Added app notebook, postBuild and requirements for binder deployment

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from atap_corpus_loader import CorpusLoader
 pn.extension('filedropper', notifications=True)
 
 
-ROOT_DIR: str = "root_dir"
+ROOT_DIR: str = "user_root"
 
 
 def get_user_directory(session_context):
@@ -22,16 +22,6 @@ def get_user_directory(session_context):
     return user_dir
 
 
-def user_view():
-    session_context = pn.state.curdoc.session_context
-    user_dir = get_user_directory(session_context)
-
-    corpus_loader: CorpusLoader = CorpusLoader(user_dir, include_meta_loader=True, include_oni_loader=True,
-                                               run_logger=True)
-
-    return corpus_loader.servable()
-
-
 def cleanup_dir(directory):
     if os.path.exists(directory):
         shutil.rmtree(directory)
@@ -42,8 +32,20 @@ def cleanup(session_context):
     cleanup_dir(user_dir)
 
 
-if __name__ == "__main__":
+def user_view():
     pn.state.on_session_destroyed(cleanup)
-    cleanup_dir(ROOT_DIR)
-    os.makedirs(ROOT_DIR, exist_ok=True)
+    session_context = pn.state.curdoc.session_context
+    user_dir = get_user_directory(session_context)
+
+    corpus_loader: CorpusLoader = CorpusLoader(user_dir, include_meta_loader=True, include_oni_loader=True,
+                                               run_logger=True)
+
+    return corpus_loader.servable()
+
+
+def runserver():
     pn.serve(user_view)
+
+
+if __name__ == "__main__":
+    runserver()
