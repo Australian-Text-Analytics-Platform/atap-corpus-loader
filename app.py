0fc4004b180b77036faed09fc1cf4a0c5ce0d483
Hamish Croser <h.croser1@gmail.com>
2024-10-18 17:04:15 +1100

    Adjusted app for deployment with no shared filespace

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
 import shutil
-import uuid
 
 import panel as pn
 
@@ -9,44 +8,31 @@ from atap_corpus_loader import CorpusLoader
 pn.extension('filedropper', notifications=True)
 
 
-ROOT_DIR: str = "user_root"
-
-
-def get_user_directory(session_context):
-    session_id = str(uuid.uuid4())
-    user_dir = os.path.join(ROOT_DIR, session_id)
-
-    os.makedirs(user_dir, exist_ok=True)
-
-    session_context.user_dir = user_dir
-    return user_dir
+ROOT_DIR: str = "corpus_files"
 
 
 def cleanup_dir(directory):
     if os.path.exists(directory):
-        shutil.rmtree(directory)
+        for filename in os.listdir(directory):
+            file_path = os.path.join(directory, filename)
+            try:
+                if os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+                else:
+                    os.remove(file_path)
+            except Exception:
+                continue
 
 
-def cleanup(session_context):
-    user_dir = session_context.user_dir
-    cleanup_dir(user_dir)
+def cleanup_session(session_context):
+    cleanup_dir(ROOT_DIR)
 
 
 def user_view():
-    pn.state.on_session_destroyed(cleanup)
-    # session_context = pn.state.curdoc.session_context
-    # user_dir = get_user_directory(session_context)
-    user_dir = ROOT_DIR
-
-    corpus_loader: CorpusLoader = CorpusLoader(user_dir, include_meta_loader=True, include_oni_loader=True,
+    pn.state.on_session_destroyed(cleanup_session)
+    corpus_loader: CorpusLoader = CorpusLoader(ROOT_DIR,
+                                               include_meta_loader=True,
+                                               include_oni_loader=True,
                                                run_logger=True)
 
     return corpus_loader.servable()
-
-
-def runserver():
-    pn.serve(user_view)
-
-
-if __name__ == "__main__":
-    runserver()
